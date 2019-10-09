d534376a1710a38403757468acee713c7e9907f9
Sulabh Kumar <sulabhkumar0@gmail.com>
2019-10-09 11:17:29 +0530

    Weather Station(observer Pattern)

---
+++ src/WeatherData.java
@@ -0,0 +1,42 @@
+import java.util.ArrayList;
+
+public class WeatherData implements Subject{
+    private ArrayList<Observer> observers;
+    private float temperature;
+    private float humidity;
+    private float pressure;
+
+    public WeatherData(){
+        observers = new ArrayList<Observer>();
+    }
+    @Override
+    public void registerObserver(Observer o) {
+        observers.add(o);
+
+    }
+
+    @Override
+    public void removeObserver(Observer o) {
+    int i = observers.indexOf(o);
+    if(i >= 0){
+       observers.remove(i);
+    }
+    }
+
+    @Override
+    public void notifyObserver() {
+        for(Observer observer: observers){
+            observer.update(temperature,humidity,pressure);
+        }
+    }
+
+    public void measurementsChange(){
+        notifyObserver();
+    }
+    public void setMeasurements(float tempreture, float humidity, float pressure){
+        this.temperature = tempreture;
+        this.humidity =humidity;
+        this.pressure = pressure;
+        measurementsChange();
+    }
+}
